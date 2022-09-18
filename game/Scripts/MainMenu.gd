e7341fc66229d4fca14833ba5c45ae979d8d031e
Ben Moeller <benlynnmoeller@gmail.com>
2022-09-17 20:04:03 -0500

    Adding initial options menu This includes: A menu with multiple tabs A save file function so user's prefereneces load when the game is launched again As well as a new Loader folder specifically made for scripts that launch when game is run

--- game/Scripts/MainMenu.gd
+++ game/Scripts/MainMenu.gd
@@ -1,10 +1,7 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Member Variables
+onready var settingsMenu = $SettingsMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,22 +12,22 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
+#function that is called when the start button is pressed
 func _on_Start_pressed():
 	pass # Replace with function body.
 
-
+#function that is called when the options button is pressed
 func _on_Options_pressed():
-	pass # Replace with function body.
+	settingsMenu.popup_centered()
 
-
+#function that is called when the market button is pressed
 func _on_Market_pressed():
 	pass # Replace with function body.
 
-
+#function that is called when the tests button is pressed
 func _on_Tests_pressed():
 	pass # Replace with function body.
 
-
+#function that is called when the quit button is pressed
 func _on_Quit_pressed():
 	get_tree().quit()
