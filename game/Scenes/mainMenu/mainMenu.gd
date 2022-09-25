a6a62190e98754394bfe0e222283c91688284239
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2022-09-24 23:57:22 -0500

    Add Documentation
    Commented functions for joinCode and added all known faults

--- game/Scenes/mainMenu/mainMenu.gd
+++ game/Scenes/mainMenu/mainMenu.gd
@@ -4,6 +4,7 @@
 * Date Created - 9/16/2022
 * Date Revisions:
 	9/17/2022 - Added options menu functionality
+	9/18/2022 - Added join code functionality
 	9/21/2022 - Fixing issue with fps label not working correctly
 """
 
@@ -49,6 +50,9 @@ func _on_Start_pressed():
 * @post Scene change to options menu
 * @param None
 * @return None
+* @knownFaults pop-up size and location depends not only on whether the screen is windowed or fullscreen,
+*   but also whether the market has been entered in the current iteration of the game
+* @knownFaults animated fire does not anchor to its position when the screen is adjusted
 */
 """
 func _on_Options_pressed():
@@ -56,17 +60,25 @@ func _on_Options_pressed():
 
 """
 /*
-* @pre Button is pressed
+* @pre Market Button is pressed
 * @post Scene change to store scene
 * @param None
 * @return None
+* @knownFaults resets join code to default (XXXX)
 */
 """
 #When button pressed switches to Store scene
 func _on_Market_pressed():
 	SceneTrans.change_scene("res://StoreElements/StoreVars.tscn")
 
-
+"""
+/*
+* @pre Tests Button is pressed
+* @post not yet implemented
+* @param None
+* @return None
+*/
+"""
 func _on_Tests_pressed():
 	pass # Replace with function body.
 
@@ -81,7 +93,14 @@ func _on_Tests_pressed():
 func _on_Quit_pressed():
 	get_tree().quit()
 
-
+"""
+/*
+* @pre "Get Code" Button is pressed
+* @post Generate and display string of four random letters in the "code" RichTextLabel
+* @param None
+* @return None
+*/
+"""
 func _on_GetCode_button_up():
 	var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
 				   'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
@@ -94,6 +113,14 @@ func _on_GetCode_button_up():
 	var code = letters[index1] + letters[index2] + letters[index3] + letters[index4]
 	$joinCodeContainer/code.set("text", code)
 	
+"""
+/*
+* @pre Helper function to _on_GetCode_button_up(). Only called within the context of the function
+* @post Generate a random number between 1 and 26 (inclusive)
+* @param rng, a RandomNumberGenerator class object
+* @return randomAlphabetIndex, the random number generated using rng.
+*/
+"""
 func getRandAlphInd(rng):
 		rng.randomize()
 		var randomAlphabetIndex = rng.randi_range(0, 25)
