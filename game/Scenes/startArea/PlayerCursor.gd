9a57344ce2ec14f453a5ff40ee21b9c0a2a68aab
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2022-10-02 18:57:34 -0500

    Improve feel of movement

--- game/Scenes/startArea/PlayerCursor.gd
+++ game/Scenes/startArea/PlayerCursor.gd
@@ -1,24 +1,28 @@
 extends KinematicBody2D
 
-const ACCELERATION = 30000
-const MAX_SPEED = 800
+const ACCELERATION = 25000
+const MAX_SPEED = 500
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
 # Reference: https://www.youtube.com/watch?v=TQKXU7iSWUU
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
-	velocity = velocity.normalized()
+	var input_velocity = Vector2.ZERO
+	input_velocity.x = Input.get_axis("ui_left", "ui_right")
+	input_velocity.y = Input.get_axis("ui_up", "ui_down") 
+	input_velocity = input_velocity.normalized()
 	
-	if velocity != Vector2.ZERO:
-		velocity = velocity.move_toward(velocity*MAX_SPEED, ACCELERATION*delta)
+	if input_velocity == Vector2.ZERO:
+		velocity = input_velocity.move_toward(Vector2.ZERO, FRICTION*delta)
+	elif input_velocity.x == 0:
+		velocity = input_velocity.move_toward(Vector2(0,input_velocity.y).normalized()*MAX_SPEED, ACCELERATION*delta)
+	elif input_velocity.y == 0:
+		velocity = input_velocity.move_toward(Vector2(input_velocity.x,0).normalized()*MAX_SPEED, ACCELERATION*delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
+		velocity = input_velocity.move_toward(0.7*input_velocity*MAX_SPEED, ACCELERATION*delta)
 	
-	velocity = move_and_collide(velocity*delta)
+	velocity = move_and_slide(velocity)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
