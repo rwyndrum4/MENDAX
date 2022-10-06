c42465d88084fe5b463496298ef308edf5763558
Ben Moeller <benlynnmoeller@gmail.com>
2022-10-06 17:19:12 -0500

    removing files and adding comments -removed some files that weren't being used but Godot was having issues with -added comments to my code for lettign player enter the cave

--- game/Scenes/startArea/startArea.gd
+++ game/Scenes/startArea/startArea.gd
@@ -6,22 +6,51 @@ var in_cave = false
 onready var instructions: Label = $enterDirections
 
 
-# Called when the node enters the scene tree for the first time.
+"""
+/*
+* @pre Called when the node enters the scene tree for the first time
+* @post runs all statements to initialize the scene (once)
+* @param None
+* @return None
+*/
+"""
 func _ready():
 	pass # Replace with function body.
 
-
-func _process(delta):
+"""
+/*
+* @pre Called for every frame
+* @post changes scenes if player presses enter and is in the zone
+* @param _delta -> time variable that can be optionally used
+* @return None
+*/
+"""
+func _process(_delta): #change to delta if using it
 	if in_cave:
 		if Input.is_action_just_pressed("ui_accept",false):
+			# warning-ignore:return_value_discarded
 			get_tree().change_scene("res://Scenes/startArea/EntrySpace.tscn")
 
-
-func _on_Area2D_body_entered(_body: PhysicsBody2D):
+"""
+/*
+* @pre Called when player enters the Ares2D zone
+* @post shows instructions on screen and sets in_cave to true
+* @param _body -> body of the player
+* @return None
+*/
+"""
+func _on_Area2D_body_entered(_body: PhysicsBody2D): #change to body if want to use
 	instructions.show()
 	in_cave = true
 
-
-func _on_Area2D_body_exited(_body: PhysicsBody2D):
+"""
+/*
+* @pre Called when player exits the Ares2D zone
+* @post hides instructions on screen and sets in_cave to false
+* @param _body -> body of the player
+* @return None
+*/
+"""
+func _on_Area2D_body_exited(_body: PhysicsBody2D): #change to body if want to use
 	in_cave = false
 	instructions.hide()
