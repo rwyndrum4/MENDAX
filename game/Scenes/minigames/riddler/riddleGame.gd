bcbbb2799d117da07280c1ea7d3569444d6dd952
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2022-10-14 00:19:28 -0500

    Create minigame room
    End of timer loads minigame, end of minigame timer reloads EntrySpace

---
+++ game/Scenes/minigames/riddler/riddleGame.gd
@@ -0,0 +1,83 @@
+"""
+* Programmer Name - Freeman Spray, Ben Moeller
+* Description - Code for controlling what happens in the entry space scene
+* Date Created - 10/3/2022
+* Date Revisions:
+	10/8/2022 - Added the ability to go into settings from scene with enter key
+"""
+extends Control
+
+# Member Variables
+var in_menu = false
+onready var settingsMenu = $GUI/SettingsMenu
+onready var myTimer: Timer = $GUI/Timer
+onready var timerText: Label = $GUI/Timer/timerText
+
+"""
+/*
+* @pre Called when the node enters the scene tree for the first time.
+* @post updates starts the timer
+* @param None
+* @return None
+*/
+"""
+func _ready():
+	myTimer.start(90)
+
+"""
+/*
+* @pre Called for every frame
+* @post updates timer and changes scenes if player presses enter and is in the zone
+* @param _delta -> time variable that can be optionally used
+* @return None
+*/
+"""
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta): #change to delta if used
+	check_settings()
+	timerText.text = convert_time(myTimer.time_left)
+	#DEBUG PURPOSES - REMOVE FOR FINAL GAME!!!
+	#IF YOU PRESS P -> TIMER WILL REDUCE TO 3 SECONDS
+	if Input.is_action_just_pressed("debug_key",false):
+		myTimer.start(3)
+
+"""
+/*
+* @pre Called for every frame inside process function
+* @post Opens and closes settings when escape is pressed
+* @param None
+* @return None
+*/
+"""
+func check_settings():
+	if Input.is_action_just_pressed("ui_cancel",false) and not in_menu:
+		settingsMenu.popup_centered_ratio()
+		in_menu = true
+	elif Input.is_action_just_pressed("ui_cancel",false) and in_menu:
+		settingsMenu.hide()
+		in_menu = false
+
+"""
+/*
+* @pre Called when need to convert seconds to MIN:SEC format
+* @post Returns string of current time
+* @param time_in -> float 
+* @return String (text of current time left)
+*/
+"""
+func convert_time(time_in:float) -> String:
+	var rounded_time = int(time_in)
+	var minutes: int = rounded_time/60
+	var seconds: int = rounded_time - (minutes*60)
+	return str(minutes,":",seconds)
+
+"""
+/*
+* @pre Called when the timer hits 0
+* @post Changes scene to minigame
+* @param None
+* @return String (text of current time left)
+*/
+"""
+func _on_Timer_timeout():
+	SceneTrans.change_scene("res://Scenes/startArea/EntrySpace.tscn")
