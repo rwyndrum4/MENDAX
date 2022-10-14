1e9478b5a920ab536c605e8caeaed51d7f3a8028
Ben Moeller <benlynnmoeller@gmail.com>
2022-10-14 00:27:38 -0500

    comments and shifting a file -added comments to my signals now in Loader/globalSignals.gd -deleted the signals file that I was testing with in the textbox folder (was not needed anymore) -also moved the globalSignals file from the Scenes folder to the Loader folder since that is where our other global code it

---
+++ game/Loader/globalSignals.gd
@@ -0,0 +1,25 @@
+"""
+* Programmer Name - Benjamin Moeller
+* Description - Holds signals that need to be used globally (aka emitted in one file, connected in another)
+* Date Created - 10/12/2022
+* Date Revisions:
+	10/14/2022 - Added the openMenu signal
+"""
+
+extends Node
+
+"""
+* Purpose - Send a signal when a textbox is displayed and goes away
+* Used in - textBox.gd and playerScript.gd
+* Parameter - value -> boolean (true if currently showing textbox, false if textbox is gone)
+"""
+# warning-ignore:unused_signal
+signal textbox_shift(value)
+
+"""
+* Purpose - Send a signal when the options menu is opened up
+* Used in - settingsMenu.gd and playerScript.gd
+* Parameter - value -> boolean (true if currently showing menu, false if menu is gone)
+"""
+# warning-ignore:unused_signal
+signal openMenu(value)
--- game/Scenes/globalSignals.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-# warning-ignore:unused_signal
-signal textbox_shift(value)
-# warning-ignore:unused_signal
-signal openMenu(value)
