00d9d4e5da13425a699bd20c1109bc448dc6ef7c
Ben Moeller <benlynnmoeller@gmail.com>
2022-10-17 00:21:45 -0500

    adding updated Nakama dependencies

--- game/Scenes/globalScope/ServerConnection.gd
+++ game/Scenes/globalScope/ServerConnection.gd
@@ -4,13 +4,15 @@ const KEY := "nakama_mendax"
 
 var _session: NakamaSession
 
-var _client := Nakama.create_client(KEY, "44.202.34.182", 7350, "http")
+#my server: 18.191.206.253
+#jasons server: 44.202.34.182
+var _client := Nakama.create_client(KEY, "18.191.206.253", 7350, "http")
 
 
 func authenticate_async(email:String, password:String) -> int:
 	var result := OK
 	
-	var new_session: NakamaSession = yield(_client.authenticate_email_async(email,password,null,true), "completed")
+	var new_session: NakamaSession = yield(_client.authenticate_email_async(email,password), "completed")
 	
 	if not new_session.is_exception():
 		_session = new_session
