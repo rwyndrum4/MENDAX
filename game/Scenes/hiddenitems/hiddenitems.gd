7ff6532a16117bad741e3c9c8192b64f4cf16854
mohitgarg <mohitgar2001@gmail.com>
2022-10-19 09:17:02 -0500

    Finished hidden item

--- game/Scenes/hiddenitems/hiddenitems.gd
+++ game/Scenes/hiddenitems/hiddenitems.gd
@@ -1,21 +1,31 @@
 extends Node
 
-#onready var itemarea=false;
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# Called when the node enters the scene tree for the first time.
+
+onready var itemfound=false;
 func _ready():
 	pass
 		
 
 func _on_item1area_body_entered(body:PhysicsBody2D)->void:
-	$player/Label.show()
+	if itemfound==false:
+		$player/Label.show()
 
 
 
 
-
-func _on_item1_body_entered(body):
+func _on_item1_body_entered(body:PhysicsBody2D)->void:
+	
 	$player/Label.hide()
-	$player/Label2.show()
+	if itemfound==false:
+		$player/Label2.show()
+		$item1/Sprite.show()
+		itemfound=true;
+
+
+
+func _on_item1_body_exited(body:PhysicsBody2D)->void:
+	$item1/Sprite.hide()
+	$player/Label2.hide() # Replace with function body.
+
+
+
