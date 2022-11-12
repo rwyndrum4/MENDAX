646a4bf7be5b69fc3a2974c68019b998d899d0da
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2022-11-11 18:25:53 -0600

    Create moving caterpillar
    non-hostile

---
+++ game/Scenes/minigames/arena/littleGuy.gd
@@ -0,0 +1,37 @@
+"""
+* Programmer Name - Freeman Spray
+* Description - Code that designates critter movement
+* Date Created - 11/11/2022
+* Citations - 
+* Date Revisions:
+
+"""
+extends KinematicBody2D
+
+# critter physics constants
+const ACCELERATION = 25000
+const MAX_SPEED = 500
+const FRICTION = 500
+
+# Global velocity
+var velocity = Vector2.ZERO
+
+var framesTraveled = 0
+var direction = true
+
+"""
+/*
+* @pre Called every frame
+* @post x and y velocity of the PlayerCursor element is updated to reflect the current player input (given by pressing WASD)
+* @param delta : elapsed time (in seconds) since previous frame. Should be constant across sequential calls
+* @return None
+*/
+"""
+func _physics_process(delta):
+	framesTraveled = framesTraveled + 1
+	if framesTraveled == 80:
+		direction = !direction
+	if direction: 
+		velocity = velocity.move_toward(Vector2(0,1), FRICTION*delta)
+	else:
+		velocity = velocity.move_toward(Vector2(0,-1), FRICTION*delta)
