9eeef1daf6d87016ccd6daa90f5a3d76ee2012cc
Ben Moeller <benlynnmoeller@gmail.com>
2022-12-23 17:16:45 -0600

    adding feedback to game

--- game/Scenes/minigames/rhythm/note.gd
+++ game/Scenes/minigames/rhythm/note.gd
@@ -5,15 +5,14 @@ extends Area2D
 # Member Variables
 onready var text_label = $label_holder/hit_type
 
-const TARGET_Y = 164
+const MODULATE_VALUE = 8
 const SPAWN_Y = -20
-const DIST_TO_TARGET = TARGET_Y - SPAWN_Y
-
 const LANE_ONE_SPAWN = Vector2(420, SPAWN_Y)
 const LANE_TWO_SPAWN = Vector2(550, SPAWN_Y)
 const LANE_THREE_SPAWN = Vector2(680, SPAWN_Y)
 const LANE_FOUR_SPAWN = Vector2(810, SPAWN_Y)
 
+const LABEL_SPEED = 650 #Speed of the label saying what type of score a note was
 var _speed = 800 #speed of the note
 var _hit = false #track whether the note was hit or not
 
@@ -27,7 +26,8 @@ func _physics_process(delta):
 			destroy(0)
 			get_parent().reset_combo()
 	else:
-		$label_holder.position.y -= _speed * delta
+		$label_holder.position.y -= LABEL_SPEED * delta
+		$label_holder.modulate.a8 -= MODULATE_VALUE
 
 func initialize(lane: int, new_speed: int):
 	_speed = new_speed
@@ -44,23 +44,31 @@ func destroy(score: int):
 	_hit = true
 	match score:
 		3:
-			#print("PERFECT")
 			text_label.text = "PERFECT"
 			text_label.modulate = Color("#40edb9")
 			get_parent().change_score(Save.game_data.username,300)
 		2:
-			#print("GOOD")
 			text_label.text = "GOOD"
 			text_label.modulate = Color("#537fed")
 			get_parent().change_score(Save.game_data.username,200)
 		1:
-			#print("OKAY")
 			text_label.text = "OKAY"
 			text_label.modulate = Color("#dbeb4d")
 			get_parent().change_score(Save.game_data.username,100)
 		0:
-			#print("MISSED")
 			text_label.text = "MISSED"
 			text_label.modulate = Color("#e03442")
-	#Destroy the note when hit
+	$Sprite.queue_free()
+	$CollisionShape2D.queue_free()
+	#Destroy the note when hit based on a timer
+	var destroy_timer: Timer = Timer.new()
+	destroy_timer.one_shot = true
+	destroy_timer.wait_time = 3
+	add_child(destroy_timer)
+	destroy_timer.start()
+	# warning-ignore: return_value_discarded
+	destroy_timer.connect("timeout", self, "_delete_node", [destroy_timer])
+
+func _delete_node(timer_var: Timer):
+	timer_var.queue_free()
 	queue_free()
