77b283cc89b9f3691a8cdc9649832b420428d314
Ben Moeller <benlynnmoeller@gmail.com>
2023-01-23 13:30:15 -0600

    server stuff

--- game/Scenes/globalScope/ServerConnection.gd
+++ game/Scenes/globalScope/ServerConnection.gd
@@ -41,7 +41,7 @@ signal minigame_player_spawned(id) #signal to tell if a player has arrived to a 
 signal chat_message_received(msg,type,user_sent,from_user) #signal to tell game a chat message has come in
 
 const KEY := "nakama_mendax" #key that is stored in the server
-var IP_ADDRESS: String = "3.23.17.173" #ip address of server
+var IP_ADDRESS: String = "3.142.209.196" #ip address of server
 
 var _session: NakamaSession #user session
 
