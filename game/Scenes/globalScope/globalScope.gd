2d6f4a2c8d8a242b10c3779e610ad65a6fd3d71b
Will Wyndrum <will.wyndrum@gmail.com>
2023-01-29 21:16:36 -0600

    hotbar initial commit

--- game/Scenes/globalScope/globalScope.gd
+++ game/Scenes/globalScope/globalScope.gd
@@ -16,6 +16,7 @@ onready var chat_box = $GUI/chatbox
 onready var settings_menu = $GUI/SettingsMenu
 onready var world_env = $WorldEnvironment
 onready var fps_label = $GUI/fpsLabel
+onready var hotbar = $GUI/Hotbar
 
 #Scene Paths
 var main_menu = "res://Scenes/mainMenu/mainMenu.tscn"
@@ -229,6 +230,9 @@ func initialize_settings():
 	settings_menu._on_MasterVolSlider_value_changed(Save.game_data.master_vol)
 	settings_menu._on_MusicVolSlider_value_changed(Save.game_data.music_vol)
 	settings_menu._on_SfxVolSlider_value_changed(Save.game_data.sfx_vol)
+
+func toggle_hotbar(en: bool):
+	hotbar.visible = en
 
 func initialize_world_env():
 	#Call functions to use user saved brightness and bloom values
