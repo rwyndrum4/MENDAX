f1d044b5a8ecde0732abee0a6ef1d47d43edaaca
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-02-07 20:32:33 -0600

    Implement Bezier interaction
    Pressing enter near one now lights it permanently

--- game/Scenes/FinalBoss/Bezier.gd
+++ game/Scenes/FinalBoss/Bezier.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-var _in_radius = 0
+var _in_radius = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +11,12 @@ func _input(_ev):
 	if _in_radius:
 		if Input.is_action_just_pressed("ui_accept",false) and not Input.is_action_just_pressed("ui_enter_chat"):
 			$Light2D.show()
-			$TileTexture.set("texture", "res://Assets/tiles/TilesCorrected/BezierLit.png")
+			$TileTexture.set("texture", preload("res://Assets/tiles/TilesCorrected/BezierLit.png")) # https://godotengine.org/qa/88049/how-to-change-sprite-from-code
+
+
+func _on_ActivationRadius_body_entered(body):
+	_in_radius = true
+
+
+func _on_ActivationRadius_body_exited(body):
+	_in_radius = false
