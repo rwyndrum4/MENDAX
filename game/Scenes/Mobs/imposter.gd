b9892c3a71eaa69cb38c718b6cb16fb8cd0c71f1
FatsozFTW <fatsozftw@gmail.com>
2023-02-26 22:22:41 -0600

    Imposter fixes

--- game/Scenes/Mobs/imposter.gd
+++ game/Scenes/Mobs/imposter.gd
@@ -5,18 +5,18 @@ onready var char_pos = $position
 onready var hitbox_imposter = $Area2D/playerHitbox
 onready var area2d = $Area2D
 onready var collisionbox = $CollisionShape2D
+onready var timer = $Timer
 var imposter_color
 var rng
 
 # Global velocity
 var velocity = Vector2.ZERO
-var BASE_SPEED = 0.7
+var BASE_SPEED = 250
 var BASE_ACCELERATION = 500
 
 func _ready():
 	randomize()
 	rng = randi() % 4
-	print(rng) 
 	if rng == 0:
 		imposter_color = "green"
 	if rng == 1:
@@ -31,7 +31,8 @@ func _physics_process(_delta):
 
 	#if not get_parent()._player_dead:
 	player_pos = get_parent().get_node("Player").position
-	velocity = move_and_slide(velocity.move_toward(BASE_SPEED*(player_pos - position), BASE_ACCELERATION*_delta))
+	velocity = position.direction_to(player_pos)* BASE_SPEED
+	velocity = move_and_slide(velocity)
 	control_animations(velocity)
 	#else:
 	
@@ -79,20 +80,30 @@ func control_animations(vel:Vector2):
 
 
 func _on_Area2D_body_entered(body):
-		if "Player" in body.name:
-			set_physics_process(false)
-			area2d.queue_free()
-			collisionbox.queue_free()
-			character.play("explosion")
-			var Player = get_parent().get_node("Player")
-			#enter health bar stuff here
-			Player.take_damage(10)
-			print("player got exploded 10 dmg")
-			yield(character, "animation_finished")
-			print("invert the controls")
-			
-			Player.isInverted = true
-			
-			
-			queue_free()
+	if "Player" in body.name:
+		timer.queue_free()
+		set_physics_process(false)
+		area2d.queue_free()
+		collisionbox.queue_free()
+		character.play("explosion")
+		var Player = get_parent().get_node("Player")
+		#enter health bar stuff here
+		Player.take_damage(10)
+		print("player got exploded 10 dmg")
+		yield(character, "animation_finished")
+		print("invert the controls")
+		
+		Player.isInverted = true
+		
+		
+		queue_free()
 
+
+
+func _on_Timer_timeout():
+	set_physics_process(false)
+	area2d.queue_free()
+	collisionbox.queue_free()
+	character.play("explosion")
+	yield(character, "animation_finished")
+	queue_free() # Replace with function body.
