215d1ececcf00c531007204b8e0b3bb597ae7986
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-04-09 20:28:01 -0500

    Update known bug report

--- game/Scenes/player/playerScript.gd
+++ game/Scenes/player/playerScript.gd
@@ -12,6 +12,7 @@
 	3/25/2023 - Added powerups
 * known bugs:
 	4/9/23 - Killing chandeliers with powerup causes spectator mode to incorrectly terminate in arena
+		cause could also be damage incoming from non-player sources
 """
 extends KinematicBody2D
 
